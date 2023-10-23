651fee7bd0c5aa8933f4c343800c038f11967f4e
Darwin Fegarido <darwin.fegarido@viseo.com>
2023-10-24 00:25:48 +0800

    adding converstaional agent

--- agent_conversation.py
+++ agent_conversation.py
@@ -1,56 +1,85 @@
-from langchain.agents import Tool
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import GPT4All
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.utilities import SerpAPIWrapper
+from langchain.agents import Tool, AgentType, initialize_agent, AgentExecutor, load_tools
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.agents.format_scratchpad import format_log_to_str
 from langchain import hub
-from langchain.agents import AgentExecutor
 
-import sys
-sys.path.append('./')
-from llms.hugging_facehub import HuggingFaceHubLLMS
+import dotenv
 
-# Initialize the LLM model
-hugging = HuggingFaceHubLLMS()
+dotenv.load_dotenv()
 
-# Setting Repo
-hugging.set_repo('flan')
-
-
-llm_model = hugging.llm
-
-# Initialize the SERP API wrapper
+params = {
+    "engine": "google"
+}
 search = SerpAPIWrapper()
 
-# Define tools for the agent
-tools = [
-    Tool(
-        name="Current Search",
-        func=search.run,
-        description="Useful for when you need to answer questions about current events or the current state of the world"
-    ),
-]
 
-# Create a prompt for the agent
-prompt = hub.pull("hwchase17/react-chat").partial(
+
+
+
+model_name = ('./models/mistral-7b-openorca.Q4_0.gguf')
+# model_name = './models/orca-mini-3b-gguf2-q4_0.gguf'
+
+# Callbacks support token-wise streaming
+callbacks = [StreamingStdOutCallbackHandler()]
+
+# template = """You are a funny chatbot having a conversation with a human.
+
+# {chat_history}
+# Human: {human_input}
+# AI:"""
+
+
+# prompt = PromptTemplate(template=template, input_variables=["chat_history","human_input"])
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm = GPT4All(model=model_name, 
+              backend="gptj", 
+              callbacks=callbacks, 
+              temp=0.7,
+              max_tokens=200)
+
+tools = load_tools(["serpapi"], llm=llm)
+
+prompt = hub.pull("hwchase17/react")
+prompt = prompt.partial(
     tools=render_text_description(tools),
     tool_names=", ".join([t.name for t in tools]),
 )
+llm_with_stop = llm.bind(stop=["\nObservation"])
 
-# Define the agent pipeline
 agent = {
     "input": lambda x: x["input"],
     "agent_scratchpad": lambda x: format_log_to_str(x['intermediate_steps']),
     "chat_history": lambda x: x["chat_history"]
-} | prompt | llm_model.bind(stop=["\nObservation"]) | ReActSingleInputOutputParser()
+} | prompt | llm_with_stop | ReActSingleInputOutputParser()
 
-# Initialize the memory
-memory = ConversationBufferMemory(memory_key="chat_history")
 
-# Initialize the agent executor
+
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, memory=memory)
 
-# Invoke the agent with an example input
-output = agent_executor.invoke({"input": "hi, i am bob"})['output']
-print(output)
+
+while True:
+    question = input(">> ")
+    if question == 'exit':
+            break
+    if question == ' ' or len(question) == 0:
+        continue
+    try:
+        
+        res = agent_executor.invoke({"input":question})
+        print(res['output'])
+    except Exception as e:
+        print(str(e))
+    finally:
+        continue
+
+
+
--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,32 @@
-from langchain.llms import OpenAI
-from integrations.hf_llms import HuggingFaceHubLLMS
+# This file is to Q and A base on the file load 
+from dotenv import load_dotenv
+from langchain.llms import HuggingFacePipeline
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.document_loaders import TextLoader, DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma, FAISS
+from langchain.chains import RetrievalQA    
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
-llm_openai = OpenAI(temperature=0.9, )
-# llm_hf = HuggingFaceHubLLMS(temp=0.9, max_length=512).llm
+load_dotenv()
 
 
-question = 'what are the best food in the philippines'
+# llms
+repo_id = "facebook/mbart-large-50-many-to-many-mmt"
+model_path = 'test/save_model/facebook-translator'
 
-print('testing')
 
-output = llm_openai(question)
-print(output)
 
-# output = llm_hf(question)
-# print(output)
+tokenizer = AutoTokenizer.from_pretrained(repo_id)
+model = AutoModelForCausalLM.from_pretrained(repo_id)
+
+tokenizer.save_pretrained(save_directory=model_path)
+model.save_pretrained(save_directory=model_path)
+
+article_hi = 'Certainly. To provide additional context, the idea of the "Kingdom of Maharlika" as proposed by Ferdinand Marcos has been met with criticism and controversy for several reasons'
+
+model.src_lang = "en_XX"
+encode = tokenizer.encode(article_hi + tokenizer.eos_token, return_tensors='pt')
+generated_tokens = model.generate(encode, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+t = tokenizer.decode(generated_tokens, skip_special_tokens=True)
+print(t)
