4b48747f04e92c66231349cf8a597f5be46694ae
Darwin Fegarido <darwin.fegarido@viseo.com>
2023-10-25 00:38:33 +0800

    adding tools

--- agent_conversation.py
+++ agent_conversation.py
@@ -1,24 +1,19 @@
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.llms import GPT4All
+from langchain.llms import GPT4All, HuggingFaceHub, HuggingFacePipeline
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.utilities import SerpAPIWrapper
 from langchain.agents import Tool, AgentType, initialize_agent, AgentExecutor, load_tools
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.agents.format_scratchpad import format_log_to_str
-from langchain import hub
+from langchain.prompts import MessagesPlaceholder
+
 
 import dotenv
 
 dotenv.load_dotenv()
-
-params = {
-    "engine": "google"
-}
-search = SerpAPIWrapper()
-
 
 
 
@@ -29,41 +24,60 @@ model_name = ('./models/mistral-7b-openorca.Q4_0.gguf')
 # Callbacks support token-wise streaming
 callbacks = [StreamingStdOutCallbackHandler()]
 
-# template = """You are a funny chatbot having a conversation with a human.
 
-# {chat_history}
-# Human: {human_input}
-# AI:"""
-
-
-# prompt = PromptTemplate(template=template, input_variables=["chat_history","human_input"])
-
-memory = ConversationBufferMemory(memory_key="chat_history")
 
 llm = GPT4All(model=model_name, 
               backend="gptj", 
               callbacks=callbacks, 
               temp=0.7,
-              max_tokens=200)
+              max_tokens=2048)
 
-tools = load_tools(["serpapi"], llm=llm)
+tool_names = ["serpapi", 'llm-math', 'wikipedia']
+tools = load_tools(tool_names, llm=llm)
 
-prompt = hub.pull("hwchase17/react")
-prompt = prompt.partial(
-    tools=render_text_description(tools),
-    tool_names=", ".join([t.name for t in tools]),
-)
-llm_with_stop = llm.bind(stop=["\nObservation"])
+# Slightly tweak the instructions from the default agent
+format_instructions = """Use the following format:
 
-agent = {
-    "input": lambda x: x["input"],
-    "agent_scratchpad": lambda x: format_log_to_str(x['intermediate_steps']),
-    "chat_history": lambda x: x["chat_history"]
-} | prompt | llm_with_stop | ReActSingleInputOutputParser()
+Chat History:
+[Here, you can record the entire conversation history, including user inputs and AI responses, to maintain context.]
+
+User: [User's previous input]
+AI: [AI's previous response]
+
+Question: [Ask your next question or share your thoughts.]
+
+Thought: [Take a moment to consider the best way to help you.]
+
+Action: [Decide on the most suitable action to take, should be one of [{tool_names}]]
+
+Action Input: [What to instruct the AI Action representative]
+
+Observation: [Here's what I found or what I can do for you.]
+
+... (this Thought/Action/Action Input/Observation can repeat N times)
+
+Thought: [I now know the final answer. User can't see any of my observations, API responses, links, or tools.]
+
+Final Answer: [The final answer to the original input question with the right amount of detail]
+
+When responding with your Final Answer, remember that the person you are responding to CANNOT see any of your Thought/Action/Action Input/Observations, so if there is any relevant information there you need to include it explicitly in your response."""
 
 
+agent_kwargs = {
+    "extra_prompt_messages": ["{memory}"], 
+    "format_instructions": format_instructions
+}
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, memory=memory)
+memory = ConversationBufferMemory(memory_key="chat_history")
+agent = initialize_agent(tools=tools, 
+                         llm=llm, 
+                         agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, 
+                         verbose=False, 
+                         early_stopping_method='generate', 
+                         max_iterations=5,
+                         memory=memory, 
+                         agent_kwargs=agent_kwargs
+                        )
 
 
 while True:
@@ -73,9 +87,10 @@ while True:
     if question == ' ' or len(question) == 0:
         continue
     try:
-        
-        res = agent_executor.invoke({"input":question})
-        print(res['output'])
+        agent.run(question)
+        print("\n")
+        print(memory.json())
+        print(' ')
     except Exception as e:
         print(str(e))
     finally:
