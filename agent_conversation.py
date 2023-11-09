fb21dfd843f6f76228b762334a2968a518badc5b
Darwin Fegarido <darwin.fegarido@viseo.com>
2023-11-10 02:10:06 +0800

    Updating agent conversational

--- agent_conversation.py
+++ agent_conversation.py
@@ -1,100 +1,148 @@
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.llms import GPT4All, HuggingFaceHub, HuggingFacePipeline
+import os
+from langchain.memory import ConversationBufferMemory, SQLChatMessageHistory
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.utilities import SerpAPIWrapper
-from langchain.agents import Tool, AgentType, initialize_agent, AgentExecutor, load_tools
-from langchain.tools.render import render_text_description
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.prompts import MessagesPlaceholder
-
-
+from langchain.llms import GPT4All
+from langchain.agents import  AgentType, initialize_agent, load_tools
 import dotenv
 
 dotenv.load_dotenv()
 
 
-
-
 model_name = ('./models/mistral-7b-openorca.Q4_0.gguf')
-# model_name = './models/orca-mini-3b-gguf2-q4_0.gguf'
+
+verbose = eval(os.getenv('VERBOSE', True))
 
 # Callbacks support token-wise streaming
 callbacks = [StreamingStdOutCallbackHandler()]
 
-
-
 llm = GPT4All(model=model_name, 
-              backend="gptj", 
-              callbacks=callbacks, 
-              temp=0.7,
-              max_tokens=2048)
+              callbacks=callbacks,
+              temp=0.7, 
+              top_k=40, 
+              top_p=0.4, 
+              repeat_penalty=1.18, 
+              repeat_last_n=64, 
+              n_batch=8, 
+              n_predict=None,
+              max_tokens=2048,
+              verbose=True,
+              streaming=True,
+              n_threads=20
+              )
 
 tool_names = ["serpapi", 'llm-math', 'wikipedia']
 tools = load_tools(tool_names, llm=llm)
 
 # Slightly tweak the instructions from the default agent
-format_instructions = """Use the following format:
+format_instructions = """
+Use the following format:
 
 Chat History:
 [Here, you can record the entire conversation history, including user inputs and AI responses, to maintain context.]
 
-User: [User's previous input]
+User: [User's current input]
 AI: [AI's previous response]
 
-Question: [Ask your next question or share your thoughts.]
+Question: [Share your thoughts.]
 
 Thought: [Take a moment to consider the best way to help you.]
 
-Action: [Decide on the most suitable action to take, should be one of [{tool_names}]]
+Action: [Decide on the most suitable action to take, use base knowledge or use tools if needed]
 
 Action Input: [What to instruct the AI Action representative]
 
-Observation: [Here's what I found or what I can do for you.]
+Observation: [Here's what I found or what I can do for you. User can't see any of my observations, API responses, links, or tools.]
 
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 
-Thought: [I now know the final answer. User can't see any of my observations, API responses, links, or tools.]
+Thought: [I now know the final answer. User can't see any of my observations, API responses, links, or tools. Always limit its token to minimize the context window.]
 
 Final Answer: [The final answer to the original input question with the right amount of detail]
 
-When responding with your Final Answer, remember that the person you are responding to CANNOT see any of your Thought/Action/Action Input/Observations, so if there is any relevant information there you need to include it explicitly in your response."""
+When responding with your Final Answer, remember that the person you are responding to CANNOT see any of your Thought/Action/Action Input/Observations, so if there is any relevant information there you need to include it explicitly in your response.
+"""
+
+def handle_error(e):
+    # Handle the parsing error gracefully, you can add more specific logic if needed
+    [*_, response] = str(e).split('AI: ')
+    # if "\n" in response:
+    #     [*_, response] = response.split('\n')
+    # breakpoint()
+    return response
+
+session_id = "my-session"
+connection_string = "sqlite:///sqlite.db"
+
+chat_memory = SQLChatMessageHistory(session_id=session_id, connection_string=connection_string)
+
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=chat_memory, return_messages=True)
+
+prefix = """Have a conversation with a human, 
+            answering the following questions as best you can. 
+            You have access to the following tools:"""
+suffix = """Begin!"
+
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
 
 
 agent_kwargs = {
-    "extra_prompt_messages": ["{memory}"], 
-    "format_instructions": format_instructions
+    "format_instructions": format_instructions,
+    "prefix": prefix,
+    "suffix": suffix,
+    "input_variables": ["input", "chat_history", "agent_scratchpad"]
 }
 
-memory = ConversationBufferMemory(memory_key="chat_history")
-agent = initialize_agent(tools=tools, 
-                         llm=llm, 
-                         agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, 
-                         verbose=False, 
-                         early_stopping_method='generate', 
-                         max_iterations=5,
-                         memory=memory, 
-                         agent_kwargs=agent_kwargs
-                        )
+agent_chain = initialize_agent(tools=tools, 
+                                llm=llm, 
+                                agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
+                                verbose=verbose, 
+                                early_stopping_method='generate', 
+                                max_iterations=2,
+                                memory=memory, 
+                                agent_kwargs=agent_kwargs,
+                                handle_parsing_errors=handle_error,
+                            )
 
 
-while True:
-    question = input(">> ")
-    if question == 'exit':
-            break
-    if question == ' ' or len(question) == 0:
+questions = [
+    'who is michael jackson',
+    # 'what name of his jordans wife',
+    # 'whats the birthday of jordans wife'
+]
+
+
+
+# Interaction loop
+for question in [*questions, 'exit']:
+
+    print(' ')
+    print('Question: ', question)
+    # User input
+    # question = input("You: ")
+    # Exit condition
+    if question.lower() == 'exit':
+        break
+    
+    # Skip empty inputs
+    if question.strip() == '':
         continue
+    
     try:
-        agent.run(question)
-        print("\n")
-        print(memory.json())
-        print(' ')
+        # Get response from the agent
+        response = agent_chain.run(input=question)
+        print('Question: ', question)
+
+        # Print AI response
+        print('AI response: ', response)
+        
     except Exception as e:
-        print(str(e))
+        # Handle parsing errors
+        print(f"err: {str(e)}")
     finally:
         continue
+
 
 
 
