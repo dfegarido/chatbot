f75adc65a775425f3805f7c091f6b808c700aa9c
Darwin Fegarido <darwin.fegarido@viseo.com>
2024-11-09 22:03:05 +0800

    update index html

--- api/client.py
+++ api/client.py
@@ -1,15 +1,22 @@
 import http.server
 import socketserver
 import os
+from dotenv import load_dotenv
+import socket
+
+load_dotenv()
 
 # Set the directory to the folder containing your HTML file
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.getcwd())
 
 # Set up the request handler to serve files
 Handler = http.server.SimpleHTTPRequestHandler
 
 # Start the server on port 8000
-PORT = 8000
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving on port {PORT}...")
+HOST = os.getenv('CLIENT_HOST', '0.0.0.0')
+PORT = int(os.getenv('CLIENT_PORT', 5000))
+
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print(" ")
+    print(f"Serving on http://{HOST}:{PORT}")
     httpd.serve_forever()
